8bfb3ec766c8ce1d46d93fdd5645029fcbcae057
Jason Alvarado <jasonalvarado410@gmail.com>
2020-12-10 23:16:08 -0800

    adding divs to the canvas using event listeners

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,27 +1,92 @@
 const sketchGame = () => {
-    //16x16 grid of divs. Most likely put in its own function when functionality for user size is implemented 
-    //const mainSquare = document.querySelector('.square');
-    let divArray = [];
     const square = document.querySelector('.square');
-    const gridSize = 22;
+    const input = document.querySelector('input');
+    const submit = document.querySelector('#submit');
+    let userGrid = 16;
+    let divArray = [];
+    let gridSize = userGrid ** 2;
 
-    for (let i =0; i < gridSize * gridSize; i++) {
-        const newSquare = document.createElement('div');
-        newSquare.className = 'small-square';
-        newSquare.style.height = `${Math.round(square.clientHeight/gridSize)}px`
-        newSquare.style.width = `${Math.round(square.clientWidth/gridSize)}px`
-        divArray[i] = newSquare;
-        square.append(newSquare);
-    }
-    console.log(divArray[0]);
-
-    divArray.forEach(square => {
-        square.addEventListener('mouseover', (e) => {
-             console.log('hovered');
-             square.style.backgroundColor = 'black';
+    const userGridSize = () => {
+        const button = document.querySelector('button');
+        button.addEventListener('click', () => {
+            userGrid = input.value;
+            gridSize = userGrid ** 2;
+            eraseGrid();
+            generateGrid();
+            activateGrid();
         });
-    });
-    
+    }
+
+    const eraseGrid = () => {
+        for (let i = 0; i < divArray.length; i++) {
+            square.removeChild(divArray[i]);
+        }
+        divArray.length = 0;
+    }
+
+    const generateGrid = () => {
+        const height = `${Math.round(square.clientHeight/userGrid)}px`
+        const width = `${Math.round(square.clientWidth/userGrid)}px`
+        for (let i = 0; i < gridSize; i++) {
+            const newSquare = document.createElement('div');
+            newSquare.classList.add('small-square');
+            newSquare.style.height = height;
+            newSquare.style.width = width;
+            divArray[i] = newSquare;
+            square.append(newSquare);
+        }
+    }
+
+    const activateGrid = () => {
+        divArray.forEach(square => {
+            square.addEventListener('mouseover', () => {
+                square.style.backgroundColor = 'black';
+            });
+        });
+    }
+
+    const getRandomColor = () => {
+        const red = Math.floor(Math.random() * 256/2);
+        const green = Math.floor(Math.random() * 256/2);
+        const blue = Math.floor(Math.random() * 256/2);
+        return `rgb(${red}, ${green}, ${blue})`;
+    }
+
+    const rainbow = () => {
+        divArray.forEach(square => {
+            square.addEventListener('mouseover', () => {
+                square.style.backgroundColor = getRandomColor();
+            });
+        });
+    }
+
+    const colorChanges = () => {
+        const colors = document.getElementsByName('color');
+        colors.forEach(color => {
+            color.addEventListener('click', (e) => {
+                if (e.target.value === 'black') {
+                    activateGrid();
+                } else if (e.target.value === 'rainbow') {
+                    rainbow();
+                }
+            });
+        });
+    }
+
+    const clearBoard = () => {
+        const erase = document.querySelector('#erase');
+        erase.addEventListener('click', () => {
+            divArray.forEach(square => {
+                square.style.backgroundColor = 'white';
+            });
+        });
+    }
+
+    userGridSize();
+    generateGrid();
+    activateGrid();
+    colorChanges();
+    clearBoard();
 }
 
 sketchGame();
